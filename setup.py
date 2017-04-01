d5241b3c51f184705d429373594381fcd3f004cf
michaelkandziora <michael@kandziora.local>
2017-04-01 20:18:47 +0200

    Bump package version for layered release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="envdiff",
-    version="0.1.0",
+    version="0.2.0",
     description="Compare environment-file keys without exposing values",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
--- src/envdiff/__init__.py
+++ src/envdiff/__init__.py
@@ -1,3 +1,3 @@
 """Key-safe environment file comparison."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
