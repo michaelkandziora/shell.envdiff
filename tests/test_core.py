6fc149eb097be969c88ecf0de829b3d660b86c4e
michaelkandziora <michael@kandziora.local>
2017-04-10 22:32:41 +0200

    Cover single quoted comment suffix

--- tests/test_core.py
+++ tests/test_core.py
@@ -44,6 +44,9 @@ class ParseTests(unittest.TestCase):
     def test_closed_quote_allows_only_a_comment_suffix(self):
         self.assertEqual(parse('A="one" # note\n'), {"A": "one"})
 
+    def test_single_quote_allows_comment_suffix_without_value_leakage(self):
+        self.assertEqual(parse("A='one' # note\n"), {"A": "one"})
+
     def test_empty_quoted_value_is_distinct_valid_syntax(self):
         self.assertEqual(parse('A=""\nB=\'\'\n'), {"A": "", "B": ""})
 
