24abb138f94f791fce825c788022beb9c2f89d5c
michaelkandziora <michael@kandziora.local>
2017-08-04 21:38:22 +0200

    Align command version expectation with release

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1315,7 +1315,7 @@ class CommandTests(unittest.TestCase):
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 universal_newlines=True)
         self.assertEqual(result.returncode, 0)
-        self.assertEqual(result.stdout.strip(), "0.5.0")
+        self.assertEqual(result.stdout.strip(), "0.6.0")
 
     def test_invalid_utf8_is_a_controlled_error(self):
         directory = tempfile.mkdtemp()
