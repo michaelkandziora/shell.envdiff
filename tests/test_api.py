a87358a8f2f5b760b646cbee051f7ffb86c17d08
michaelkandziora <michael@kandziora.local>
2017-10-07 21:37:55 +0200

    Cover public single and multi mapping comparisons

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,18 @@
 import unittest
 
-from envdiff import ComparisonResult, Difference, TargetResult
+from envdiff import ComparisonResult, Difference, TargetResult, compare_mappings
 
 
 class PublicResultTests(unittest.TestCase):
+    def test_public_api_compares_one_mapping_target(self):
+        result = compare_mappings({"A": "one", "B": "one"}, [{"A": "two", "C": "two"}])
+        self.assertEqual(result.targets[0].difference,
+                         Difference(("B",), ("C",), ("A",)))
+
+    def test_public_api_preserves_multi_target_ordinals(self):
+        result = compare_mappings({"A": "one"}, [{"A": "two"}, {"A": "one"}])
+        self.assertEqual([item.target for item in result.targets], [1, 2])
+        self.assertEqual(result.targets[1].difference, Difference())
     def test_public_result_is_deeply_immutable(self):
         difference = Difference(("MISSING",), ("EXTRA",), ("CHANGED",))
         result = ComparisonResult((TargetResult(1, difference, ()),))
