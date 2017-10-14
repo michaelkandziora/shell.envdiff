2e0ce049e8f6869aed71bec0959f3db59f160df0
michaelkandziora <michael@kandziora.local>
2017-10-14 21:24:16 +0200

    Prepare envdiff 1.0.0 package metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="envdiff",
-    version="0.6.0",
+    version="1.0.0",
     description="Compare environment-file keys without exposing values",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
--- src/envdiff/__init__.py
+++ src/envdiff/__init__.py
@@ -3,7 +3,7 @@
 from .api import (ComparisonError, ComparisonResult, Difference, Source,
                   TargetResult, compare_files, compare_mappings, result_document)
 
-__version__ = "0.6.0"
+__version__ = "1.0.0"
 
 __all__ = ["ComparisonError", "ComparisonResult", "Difference", "Source", "TargetResult",
            "compare_files", "compare_mappings", "result_document"]
