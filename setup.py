85a93d328e6dac90a2ea587f5b0423f2445cdf84
cicada <cicada@android>
2026-09-24 02:16:32 -0400

    Fix envdiff distribution metadata

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ setup(
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Michael Kandziora",
-    author_email="michael@kandziora.local",
-    url="https://example.invalid/envdiff",
+    url="https://github.com/michaelkandziora/shell.envdiff",
     license="MIT",
     package_dir={"": "src"},
     packages=["envdiff"],
     python_requires=">=3.6",
     entry_points={"console_scripts": ["envdiff=envdiff.cli:main"]},
     classifiers=[
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: MIT License",
     ],
 )
